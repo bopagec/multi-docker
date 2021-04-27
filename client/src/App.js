ac3102e4f3d983472bc9e76547cfa6daedab0c24
bopagep <pohodhika.bopage@uk.fujitsu.com>
2021-04-27 16:22:43 +0100

    correction

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
-// default react app listen on port 3000
+
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React 3
+          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
